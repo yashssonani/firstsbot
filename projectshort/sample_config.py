866afcd3ba22f76dcabac919909be535079a1a56
sahilpatel12345 <79982389+sahilpatel12345@users.noreply.github.com>
2021-03-07 14:54:29 +0530

    Update sample_config.py

--- projectshort/sample_config.py
+++ projectshort/sample_config.py
@@ -93,9 +93,9 @@ class Config:
     )
     # array to store the users who will have control (permissions)
     # in the bot
-    SUDO_USERS = set(
-        int(x) for x in get_config(
-            "SUDO_USERS",
-            should_prompt=True
-        ).split()
+#     SUDO_USERS = set(
+#         int(x) for x in get_config(
+#             "SUDO_USERS",
+#             should_prompt=True
+#         ).split()
     )
